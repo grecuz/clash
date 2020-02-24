0a119246c3c5fc8b8bdf4ad6bbf16965620c41b1
grecuz <zoica.grecu@gmail.com>
2020-02-24 16:16:16 +0200

    Add files via upload

---
+++ src/main/java/com/example/pack/springwebservice/SpringWebserviceApplication.java
@@ -0,0 +1,16 @@
+package com.example.pack.springwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class SpringWebserviceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/pack/springwebservice/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.pack.springwebservice.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users")//http://localhost:8080/users
+public class UserController {
+
+    @GetMapping
+    public String getUser(){
+        return "getUser was called";
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "createUser was called";
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "updateUser was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser(){
+        return "deleteUser was called";
+    }
+}
